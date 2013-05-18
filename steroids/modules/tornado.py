fe86250e44c6e685351848952b8e7daf35dc8d17
Lorenzo Setale <koalalorenzo@gmail.com>
2013-05-18 16:08:08 +0200

    Created Tornado module

---
+++ steroids/modules/tornado.py
@@ -0,0 +1,41 @@
+import os
+
+dependencies = [
+    "flask"
+    ]
+    
+requirements = [
+    "tornado"
+]
+
+directories = [
+    "[base]",
+    ]
+
+files = [
+    "[base]/tornado.py",
+    ]
+
+def install(basepath, name):
+    """
+        Install Tornado Module
+    """
+    
+    tornado_file = open(os.path.join(basepath,"tornado.py"), "w")
+    tornado_file.write("from tornado.wsgi import WSGIContainer\n")
+    tornado_file.write("from tornado.httpserver import HTTPServer\n")
+    tornado_file.write("from tornado.ioloop import IOLoop\n")
+    tornado_file.write("from configuration import SERVER_PORT\n")
+    tornado_file.write("from %s import app as application\n" % name)
+    tornado_file.write("\n")
+    tornado_file.write("http_server = HTTPServer(WSGIContainer(application))\n")
+    tornado_file.write("http_server.listen(SERVER_PORT)\n")
+    tornado_file.write("IOLoop.instance().start()\n")
+    tornado_file.close()
+    return
+    
+def install_examples(basepath, name):
+    """
+        Install Tornado Module with example files
+    """
+    return
