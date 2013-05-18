1b7051064bf3c824d0eafe0d99f6f4e07d3edea2
Lorenzo Setale <koalalorenzo@gmail.com>
2013-05-18 16:07:56 +0200

    Restored Flask module

--- steroids/modules/__init__.py
+++ steroids/modules/__init__.py
@@ -2,3 +2,4 @@ from steroids.modules import apscheduler
 from steroids.modules import honcho
 from steroids.modules import mongodb
 from steroids.modules import tornado
+from steroids.modules import flask
---
+++ steroids/modules/flask.py
@@ -0,0 +1,28 @@
+requirements = [
+    "flask"
+]
+
+directories = [
+    "[base]/[name]",
+    "[base]/[name]/templates",
+    "[base]/[name]/static",
+    "[base]/[name]/views",
+]
+
+files = [
+    "[base]/server.py",
+    "[base]/[name]/__init__.py",
+    "[base]/[name]/views/__init__.py",
+]
+
+def install(basepath, name):
+    """
+        Install Flask Module
+    """
+    return
+    
+def install_examples(basepath, name):
+    """
+        Install Flask Module
+    """
+    return
