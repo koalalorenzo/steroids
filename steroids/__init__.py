295851bbca6c83bacf4fcabe7d535f6c135e7fa2
Lorenzo Setale <koalalorenzo@gmail.com>
2013-05-19 14:46:45 +0200

    Added important informations about the module.

---
+++ steroids/__init__.py
@@ -0,0 +1,7 @@
+"""Steroids: tools to help developers building real-time ready, non-blocking, python web application reducing efforts and time."""
+
+__version__ = "0.3"
+__author__ = "Lorenzo Setale ( http://who.is.lorenzo.setale.me/? )"
+__author_email__ = "koalalorenzo@gmail.com"
+__license__ = "Creative Commons Attribution-ShareAlike 3.0 Unported License http://creativecommons.org/licenses/by-nd/3.0/ "
+__copyright__ = "Copyright (c) 2013, 2014 Lorenzo Setale"
