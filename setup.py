68cb2d7cc4ac4e08e12c5a19f9d41ecc3fec5668
Lorenzo Setale <koalalorenzo@gmail.com>
2013-05-19 02:26:51 +0200

    upgrade setup.py for installation and "pip package"

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import os
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+setup(name='steroids',
+      version='0.3',
+      description='',
+      author='Lorenzo Setale ( http://who.is.lorenzo.setale.me/? )',
+      author_email='koalalorenzo@gmail.com',
+      url='https://github.com/koalalorenzo/Steroids',
+      packages=['steroids'],
+      scripts=['bin/steroids']
+     )
