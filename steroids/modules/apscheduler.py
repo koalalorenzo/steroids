1f5c222b92fd813bdaac200ca5271edc824a8ac1
Lorenzo Setale <koalalorenzo@gmail.com>
2013-05-19 12:07:29 +0200

    Created APScheduler Module

---
+++ steroids/modules/apscheduler.py
@@ -0,0 +1,60 @@
+import os
+
+requirements = [
+    "apscheduler"
+]
+
+directories = [
+    "[base]",
+    ]
+
+files = [
+    "[base]/clock.py",
+    "[base]/[name]/clock.py",
+    ]
+
+def install(basepath, name):
+    """
+        Install apscheduler Module
+    """
+    
+    clock_file = open(os.path.join(basepath,"clock.py"), "w")
+    clock_file.write("from %s.clock import sched\n" % name)    
+    clock_file.write("""
+sched.start()
+while True:
+    if not sched.running:
+        break
+    sleep(30)
+        
+""")
+    clock_file.close()
+
+    nameclock_file = open(os.path.join(basepath,"%s/clock.py" % name), "w")    
+    nameclock_file.write("import sys\n")
+    nameclock_file.write("from apscheduler.scheduler import Scheduler\n")
+    nameclock_file.write("\n")
+    nameclock_file.write("sched = Scheduler()\n")
+    nameclock_file.close()
+    return
+    
+def install_examples(basepath, name):
+    """
+        Install Tornado Module with example files
+    """
+    nameclock_file = open(os.path.join(basepath,"%s/clock.py" % name), "a")    
+    nameclock_file.write("""
+# Examples APScheduler
+
+@sched.interval_schedule(minutes=5)
+def every_five_minutes():
+    print "5 minutes"
+
+@sched.cron_schedule(day='last sun')
+def some_decorated_task():
+    print "I am printed at 00:00:00 on the last Sunday of every month!"
+
+# End Examples
+""")
+    nameclock_file.close()
+    return
