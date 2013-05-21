7200f1de9c9ee5e7c27daa5e7c2ed29e38287c73
Lorenzo Setale <koalalorenzo@gmail.com>
2013-05-21 11:37:36 +0200

    Implemented correctly extraction of zip bootstrap file

--- steroids/modules/bootstrap.py
+++ steroids/modules/bootstrap.py
@@ -11,11 +11,11 @@ __help__ = """This module will install Twitter bootstrap. If you use examples, t
 requirements = [ ]
 
 directories = [
-    "[base]"
-    "[base]/[name]"
-    "[base]/[name]/static"
-    "[base]/[name]/static/bootstrap"
-    "[base]/[name]/templates"
+    "[base]",
+    "[base]/[name]",
+    "[base]/[name]/static",
+    "[base]/[name]/static/bootstrap",
+    "[base]/[name]/templates",
 ]
 
 files = [ ]
@@ -26,20 +26,30 @@ def install(basepath, name):
     """
     # http://twitter.github.io/bootstrap/assets/bootstrap.zip
     
-    bootstrap_file_path = os.path.join(basepath, name, "static/bootstrap.zip")
+    static_files_path = os.path.join(basepath, name, "static/")
+    bootstrap_file_path = os.path.join(static_files_path, "bootstrap.zip")
+    
     
     remote_file = urllib2.urlopen('http://twitter.github.io/bootstrap/assets/bootstrap.zip').read()
     localfile = open(bootstrap_file_path, 'w')
     localfile.write(remote_file)
     localfile.close()
     
+    current_dir = os.getcwd()
     bootstrap_zip = zipfile.ZipFile(bootstrap_file_path)
     for f in bootstrap_zip.namelist():
         if f.endswith('/'):
-            os.makedirs(f)
+            new_directory = os.path.join(static_files_path,f)
+            if not os.path.exists(new_directory):
+                os.makedirs(new_directory)
+            
+            os.chdir(current_dir)
+            os.chdir(new_directory)
         else:
             bootstrap_zip.extract(f)
-            
+    os.chdir(current_dir)
+    os.remove(bootstrap_file_path)
+    
     return
     
 def install_examples(basepath, name):
