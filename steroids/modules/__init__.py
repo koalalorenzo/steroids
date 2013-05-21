8195c01e422ba544d3a602aac5476803baaa0f56
Lorenzo Setale <koalalorenzo@gmail.com>
2013-05-21 11:37:20 +0200

    Added bootstrap in supported modules

--- steroids/modules/__init__.py
+++ steroids/modules/__init__.py
@@ -2,4 +2,5 @@ from steroids.modules import apscheduler
 from steroids.modules import honcho
 from steroids.modules import mongodb
 from steroids.modules import tornado
+from steroids.modules import bootstrap
 from steroids.modules import flask
