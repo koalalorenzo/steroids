792f39d398557e2c3c94f30c44d197691a96f043
Lorenzo Setale <koalalorenzo@gmail.com>
2013-05-21 14:18:23 +0200

    Increased version to 0.6.2
    Increased version to 0.6.2 ( documentation part and install scripts )

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup
 
 setup(name='steroids',
-      version='0.6.1',
+      version='0.6.2',
       description='Steroids: a set of tools to help python developers to build real-time ready, non-blocking, web application reducing efforts and time.',
       author='Lorenzo Setale ( http://who.is.lorenzo.setale.me/? )',
       author_email='koalalorenzo@gmail.com',
