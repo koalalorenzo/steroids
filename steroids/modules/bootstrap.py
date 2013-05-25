95a80d3b33e71177f71b892f6a9910abeeb9a8cf
Lorenzo Setale <koalalorenzo@gmail.com>
2013-05-25 14:26:17 +0200

    Fix that would not load sometimes the jquery code.

--- steroids/modules/bootstrap.py
+++ steroids/modules/bootstrap.py
@@ -58,7 +58,7 @@ def install(basepath, name):
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 
-        <script src="http://code.jquery.com/jquery.min.js"></script>
+        <script src="//code.jquery.com/jquery.min.js"></script>
         <script src="/static/bootstrap/js/bootstrap.js"></script>
         <link href="/static/bootstrap/css/bootstrap.css" rel="stylesheet">
         <link href="/static/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
