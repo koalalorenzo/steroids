15077fedc5a386a96fa90a80a1f683f76a31f05d
Lorenzo Setale <koalalorenzo@gmail.com>
2013-05-26 21:33:22 +0200

    Enabled topcoat module

--- steroids/modules/__init__.py
+++ steroids/modules/__init__.py
@@ -3,4 +3,5 @@ from steroids.modules import honcho
 from steroids.modules import mongodb
 from steroids.modules import tornado
 from steroids.modules import bootstrap
+from steroids.modules import topcoat
 from steroids.modules import flask
