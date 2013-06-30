f5ec7cdcaba2699c2a016c9bb24e0f652cfcb7cf
Lorenzo Setale <koalalorenzo@gmail.com>
2013-06-30 13:25:14 +0200

    Updated descriptions.

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ import os
 from setuptools import setup
 
 setup(name='steroids',
-      version='0.11.2',
-      description='Steroids: a set of tools to help python developers to build real-time ready, non-blocking, web application reducing efforts and time.',
+      version='0.11.3',
+      description='Steroids is a set of tools to help python developers to build cool stuff reducing efforts and time.',
       author='Lorenzo Setale ( http://who.is.lorenzo.setale.me/? )',
       author_email='koalalorenzo@gmail.com',
       url='https://github.com/koalalorenzo/Steroids',
