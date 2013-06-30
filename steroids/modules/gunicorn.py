1a1be754b61b164c3d9bf772cca52f452be83f99
Lorenzo Setale <koalalorenzo@gmail.com>
2013-06-30 12:47:59 +0200

    Created gunicorn support

---
+++ steroids/modules/gunicorn.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+# -*- coding=utf-8 -*-
+
+import os
+import stat   
+
+__help__ = """Gunicorn 'Green Unicorn' is a Python WSGI HTTP Server for UNIX.
+"""
+
+
+requirements = [
+    "gunicorn"
+]
+
+directories = [ ]
+
+files = [
+    "[base]/start.sh"
+]
+
+def install(basepath, name):
+    """
+        Install gunicorn module
+    """
+
+    start_file_path = os.path.join(basepath,"start.sh")
+    start_file = open(start_file_path, "a")
+    start_file.write("#!/bin/bash\n")
+    start_file.write("gunicorn server:app")
+    start_file.close()
+
+    old_perms = os.stat(start_file_path)
+    os.chmod(start_file_path, old_perms.st_mode | stat.S_IEXEC)
+
+    return
+    
+def install_examples(basepath, name):
+    """
+        Install gunicorn module examples
+    """
+    start_file = open(os.path.join(basepath,"start.sh"), "a")
+    start_file.write("##Other commands examples:\n")
+    start_file.write("# gunicorn server:app -k gevent # non blocking server\n")
+    start_file.close()
+
+    if os.path.exists(os.path.join(basepath,"Procfile")):
+        proc_file = open(os.path.join(basepath,"Procfile"), "a")
+        proc_file.write("##Other commands examples:\n")
+        proc_file.write("# web: gunicorn server:app -k gevent # non blocking server\n")
+        proc_file.close()
+
+    return
