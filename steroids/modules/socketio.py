945b84cbcd0bb35acc682c8d691604eea314ef50
Lorenzo Setale <koalalorenzo@gmail.com>
2013-07-08 01:01:01 +0200

    Improved SocketIO modules with some (not tested) examples.

--- steroids/modules/socketio.py
+++ steroids/modules/socketio.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 # -*- coding=utf-8 -*-
-
 import os
 import urllib2
 import zipfile
@@ -16,6 +15,7 @@ directories = [
     "[base]",
     "[base]/[name]",
     "[base]/[name]/static",
+    "[base]/[name]/templates",
 ]
 
 files = [ ]
@@ -33,6 +33,23 @@ def install(basepath, name):
     localfile = open(socketioclient_file_path, 'w')
     localfile.write(remote_file)
     localfile.close()
+
+    template_socketio = open(os.path.join(templates_path, "socketio.html"), "w")
+    template_socketio.write("""<html>
+    <head>
+        <title>%s {%% block title %%}{%% endblock %%}</title>
+        <meta charset="utf-8">
+        <script src="//code.jquery.com/jquery.min.js"></script>
+        <script src="/static/socket.io-client.js"></script>
+        {%% block head %%}{%% endblock %%}
+    </head>
+    <body>
+        {%% block header %%}{%% endblock %%}
+        {%% block container %%}{%% endblock %%}
+        {%% block footer %%}{%% endblock %%}
+    </body>
+</html>""" % name)
+    template_socketio.close()
     
     return
     
@@ -40,5 +57,94 @@ def install_examples(basepath, name):
     """
         Install socket.io examples. 
     """
+
+    examples_tempaltes_path = os.path.join(basepath,name,"templates","examples")
+    if not os.path.exists(examples_tempaltes_path):
+        os.mkdir(examples_tempaltes_path)
+
+    examples_template_alert_path = os.path.join(examples_tempaltes_path,"socketio")
+    if not os.path.exists(examples_template_alert_path):
+        os.mkdir(examples_template_alert_path)
+
+    template_alert_users = open(os.path.join(examples_template_alert_path, "alert.html"), "w")
+    template_alert_users.write("""{% extends "socketio.html" %}
+
+{% block container %}
+<h1>Hello world!</h1>
+<div class="hero-unit">
+    <p class="lead">
+        <a class="btn btn-large" href="javascript:void(0);" onclick="alertUsers();">alert every users connected</a>
+    <p>
+</div>
+
+<script>
+  var alerter = io.connect('http://localhost/alerter')
+  
+  alerter.on('connect', function () {
+    alert("!!!");
+  });
+
+  function alertUsers() {
+    alerter.emit("alert");
+  }
+</script>
+{% endblock %}
+
+    """)
+    template_alert_users.close()
+
+
+    views_path = os.path.join(basepath,name,"views")
+    if not os.path.exists(views_path):
+        os.mkdir(views_path)
+
+    template_alert_users = open(os.path.join(views_path, "socketioExample.py"), "w")
+    template_alert_users.write("""from %s import app
+from %s.decorators import *
+from %s.constants import *
+
+from flask import render_template
+from flask import url_for
+from flask import session
+from flask import abort
+from flask import redirect
+from flask import flash
+
+from socketio import socketio_manage
+from socketio.namespace import BaseNamespace
+
+class AlertsNamespace(BaseNamespace):
+    def __init__(self, *args, **kwargs):
+        request = kwargs.get('request', None)
+        self.ctx = None
+        if request:
+            self.ctx = current_app.request_context(request.environ)
+            self.ctx.push()
+            current_app.preprocess_request()
+            del kwargs['request']
+        super(BaseNamespace, self).__init__(*args, **kwargs)
+
+    def disconnect(self, *args, **kwargs):
+        if self.ctx:
+            self.ctx.pop()
+        super(BaseNamespace, self).disconnect(*args, **kwargs)
+
+    def on_alert():
+        self.emit("alert");
+
+@app.route('/socket.io/<path:path>')
+def run_socketio(path):
+    real_request = request._get_current_object()
+    socketio_manage(request.environ, {'/alerter': AlertsNamespace},
+            request=real_request)
+    return Response()
+
+""" % (name, name, name))
+    template_alert_users.close()
+
+    init_file = open(os.path.join(basepath, name, "__init__.py"), "a")
+    init_file.write("\nimport %s.views.socketioExample\n" % name)
+    init_file.close()
+
 
     return
