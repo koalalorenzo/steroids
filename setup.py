e2c7f9a65fa685735ca4d43bed8a8cafe3f1b3b6
Lorenzo Setale <koalalorenzo@gmail.com>
2013-07-08 15:48:07 +0200

    New version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup
 
 setup(name='steroids',
-      version='0.12.1',
+      version='0.12.2',
       description='Steroids is a set of tools to help python developers to build cool stuff reducing efforts and time.',
       author='Lorenzo Setale ( http://who.is.lorenzo.setale.me/? )',
       author_email='koalalorenzo@gmail.com',
